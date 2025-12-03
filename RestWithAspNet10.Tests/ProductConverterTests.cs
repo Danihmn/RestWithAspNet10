3e23eee62b6b9b8e03adcb93a9eb2b7c31d68685
Danihmn <daniel.bezerra.mult@gmail.com>
2025-12-02 21:24:03 -0300

    Insere testes de conversão de DTO para Model e inicia testes de conversão de listas

--- RestWithAspNet10.Tests/ProductConverterTests.cs
+++ RestWithAspNet10.Tests/ProductConverterTests.cs
@@ -70,5 +70,100 @@ namespace RestWithAspNet10.Tests
             // Assert
             resultProduct.Should().BeNull();
         }
+
+        [Fact]
+        public void Parse_ShouldConvertProductModelToProductDTOWithManualConverters ()
+        {
+            // Arrange
+            ProductModel originProductModel = new ProductModel
+            {
+                Id = 1,
+                Name = "Inspiron 15 3520",
+                Description = "intel Core i5, 512 GB, 8 GB RAM",
+                Brand = "Dell",
+                QuantityStock = 5,
+                CostPrice = 1000,
+                SalePrice = 2000
+            };
+
+            ProductDTO expectedProductDTO = new ProductDTO
+            {
+                Id = 1,
+                Name = "Inspiron 15 3520",
+                Description = "intel Core i5, 512 GB, 8 GB RAM",
+                Brand = "Dell",
+                QuantityStock = 5,
+                SalePrice = 2000
+            };
+
+            // Act
+            ProductDTO resultProduct = _converter.Parse(originProductModel);
+
+            // Assert
+            resultProduct.Should().BeEquivalentTo(expectedProductDTO, options => options.Excluding(product => product.CostPrice));
+        }
+
+        [Fact]
+        public void Parse_ShouldReturnAtNullValuesCaseAtConvertProductModelToProductDTOWithManualConverters ()
+        {
+            // Arrange
+            ProductModel originProductModel = null;
+
+            ProductDTO expectedProductModel = new ProductDTO
+            {
+                Id = 1,
+                Name = "Inspiron 15 3520",
+                Description = "intel Core i5, 512 GB, 8 GB RAM",
+                Brand = "Dell",
+                QuantityStock = 5,
+                CostPrice = 1000
+            };
+
+            // Act
+            ProductDTO resultProduct = _converter.Parse(originProductModel);
+
+            // Assert
+            resultProduct.Should().BeNull();
+        }
+
+        [Fact]
+        public void Parse_ShouldConvertProductDTOListToProductModelListWithManualConverters ()
+        {
+            // Arrange
+            List<ProductDTO> originProductDTOList = new List<ProductDTO>
+            {
+                new ProductDTO
+                {
+                    Id = 1,
+                    Name = "Inspiron 15 3520",
+                    Description = "intel Core i5, 512 GB, 8 GB RAM",
+                    Brand = "Dell",
+                    QuantityStock = 5,
+                    CostPrice = 1000
+                },
+                new ProductDTO
+                {
+                    Id = 2,
+                    Name = "Ideapad Flex 5i",
+                    Description = "intel Core i7, 256 GB, 8 GB RAM",
+                    Brand = "Lenovo",
+                    QuantityStock = 5,
+                    CostPrice = 1500
+                }
+            };
+
+            // Act
+            List<ProductModel> expectedProductModelList = _converter.ParseList(originProductDTOList);
+
+            // Assert
+            expectedProductModelList.Should().NotBeNull();
+            expectedProductModelList.Should().HaveCount(originProductDTOList.Count);
+            expectedProductModelList.Should().BeEquivalentTo(originProductDTOList, options => options.Excluding(product => product.SalePrice));
+            expectedProductModelList.Should().SatisfyRespectively(
+            originProductDTOList.Select<ProductDTO, Action<ProductModel>>(origin => expected =>
+            {
+                expected.SalePrice.Should().Be(origin.CostPrice * 2);
+            }).ToArray());
+        }
     }
 }
