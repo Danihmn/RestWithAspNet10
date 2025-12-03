5e82ffa782cc61c792dbcb3d516499adefd27da4
Danihmn <daniel.bezerra.mult@gmail.com>
2025-12-02 22:37:15 -0300

    Finaliza codificação de testes unitários dos conversores de Product

--- RestWithAspNet10.Tests/ProductConverterTests.cs
+++ RestWithAspNet10.Tests/ProductConverterTests.cs
@@ -14,6 +14,7 @@ namespace RestWithAspNet10.Tests
             _converter = new ProductConverter();
         }
 
+        #region Convertions with not null values
         [Fact]
         public void Parse_ShouldConvertProductDTOToProductModelWithManualConverters ()
         {
@@ -48,30 +49,6 @@ namespace RestWithAspNet10.Tests
         }
 
         [Fact]
-        public void Parse_ShouldReturnAtNullValuesCaseAtConvertProductDTOToProductModelWithManualConverters ()
-        {
-            // Arrange
-            ProductDTO originProductDTO = null;
-
-            ProductModel expectedProductModel = new ProductModel
-            {
-                Id = 1,
-                Name = "Inspiron 15 3520",
-                Description = "intel Core i5, 512 GB, 8 GB RAM",
-                Brand = "Dell",
-                QuantityStock = 5,
-                CostPrice = 1000,
-                SalePrice = 2000
-            };
-
-            // Act
-            ProductModel resultProduct = _converter.Parse(originProductDTO);
-
-            // Assert
-            resultProduct.Should().BeNull();
-        }
-
-        [Fact]
         public void Parse_ShouldConvertProductModelToProductDTOWithManualConverters ()
         {
             // Arrange
@@ -101,29 +78,7 @@ namespace RestWithAspNet10.Tests
 
             // Assert
             resultProduct.Should().BeEquivalentTo(expectedProductDTO, options => options.Excluding(product => product.CostPrice));
-        }
-
-        [Fact]
-        public void Parse_ShouldReturnAtNullValuesCaseAtConvertProductModelToProductDTOWithManualConverters ()
-        {
-            // Arrange
-            ProductModel originProductModel = null;
-
-            ProductDTO expectedProductModel = new ProductDTO
-            {
-                Id = 1,
-                Name = "Inspiron 15 3520",
-                Description = "intel Core i5, 512 GB, 8 GB RAM",
-                Brand = "Dell",
-                QuantityStock = 5,
-                CostPrice = 1000
-            };
-
-            // Act
-            ProductDTO resultProduct = _converter.Parse(originProductModel);
-
-            // Assert
-            resultProduct.Should().BeNull();
+            resultProduct.SalePrice.Should().Be(expectedProductDTO.SalePrice);
         }
 
         [Fact]
@@ -165,5 +120,102 @@ namespace RestWithAspNet10.Tests
                 expected.SalePrice.Should().Be(origin.CostPrice * 2);
             }).ToArray());
         }
+
+        [Fact]
+        public void Parse_ShouldConvertProductModelListToProductDTOListWithManualConverters ()
+        {
+            // Arrange
+            List<ProductModel> originProductModelList = new List<ProductModel>
+            {
+                new ProductModel
+                {
+                    Id = 1,
+                    Name = "Inspiron 15 3520",
+                    Description = "intel Core i5, 512 GB, 8 GB RAM",
+                    Brand = "Dell",
+                    QuantityStock = 5,
+                    CostPrice = 1000,
+                    SalePrice = 2000
+                },
+                new ProductModel
+                {
+                    Id = 2,
+                    Name = "Ideapad Flex 5i",
+                    Description = "intel Core i7, 256 GB, 8 GB RAM",
+                    Brand = "Lenovo",
+                    QuantityStock = 5,
+                    CostPrice = 1500,
+                    SalePrice = 3000
+                }
+            };
+
+            // Act
+            List<ProductDTO> expectedProductDTOList = _converter.ParseList(originProductModelList);
+
+            // Assert
+            expectedProductDTOList.Should().NotBeNull();
+            expectedProductDTOList.Should().HaveCount(originProductModelList.Count);
+            expectedProductDTOList.Should().BeEquivalentTo(originProductModelList, options => options.Excluding(product => product.CostPrice));
+            expectedProductDTOList.Should().SatisfyRespectively(
+            originProductModelList.Select<ProductModel, Action<ProductDTO>>(origin => expected =>
+            {
+                expected.SalePrice.Should().Be(origin.SalePrice);
+            }).ToArray());
+        }
+        #endregion
+
+        #region Convertions with null values
+        [Fact]
+        public void Parse_ShouldReturnAtNullValuesCaseAtConvertProductDTOToProductModelWithManualConverters ()
+        {
+            // Arrange
+            ProductDTO originProductDTO = null;
+
+            // Act
+            ProductModel resultProduct = _converter.Parse(originProductDTO);
+
+            // Assert
+            resultProduct.Should().BeNull();
+        }
+
+        [Fact]
+        public void Parse_ShouldReturnAtNullValuesCaseAtConvertProductModelToProductDTOWithManualConverters ()
+        {
+            // Arrange
+            ProductModel originProductModel = null;
+
+            // Act
+            ProductDTO resultProduct = _converter.Parse(originProductModel);
+
+            // Assert
+            resultProduct.Should().BeNull();
+        }
+
+        [Fact]
+        public void Parse_ShouldReturnAtNullValuesCaseAtConvertProductDTOListToProductModelListWithManualConverters ()
+        {
+            // Arrange
+            List<ProductDTO> originProductDTOList = null;
+
+            // Act
+            List<ProductModel> resultProduct = _converter.ParseList(originProductDTOList);
+
+            // Assert
+            resultProduct.Should().BeNull();
+        }
+
+        [Fact]
+        public void Parse_ShouldReturnAtNullValuesCaseAtConvertProductModelListToProductDTOListWithManualConverters ()
+        {
+            // Arrange
+            List<ProductModel> originProductModelList = null;
+
+            // Act
+            List<ProductDTO> resultProduct = _converter.ParseList(originProductModelList);
+
+            // Assert
+            resultProduct.Should().BeNull();
+        }
+        #endregion
     }
 }
