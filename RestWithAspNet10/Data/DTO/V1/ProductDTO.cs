c0027517bd09fd67b0cac8e1b8d40713d99d55f3
Danihmn <daniel.bezerra.mult@gmail.com>
2025-12-03 23:07:30 -0300

    Adiciona JsonPropertyName para a API

--- RestWithAspNet10/Data/DTO/V1/ProductDTO.cs
+++ RestWithAspNet10/Data/DTO/V1/ProductDTO.cs
@@ -1,13 +1,25 @@
-﻿namespace RestWithAspNet10.Data.DTO.V1
+﻿using System.Text.Json.Serialization;
+
+namespace RestWithAspNet10.Data.DTO.V1
 {
     public class ProductDTO
     {
+        [JsonPropertyName("identifier")]
         public long Id { get; set; }
+
         public string? Name { get; set; }
+
         public string? Description { get; set; }
+
         public string? Brand { get; set; }
+
+        [JsonPropertyName("quantity_stock")]
         public int QuantityStock { get; set; }
+
+        [JsonPropertyName("cost_price")]
         public decimal CostPrice { get; set; }
+
+        [JsonPropertyName("sale_price")]
         public decimal SalePrice { get; set; }
     }
 }
