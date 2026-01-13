a8f05900a18e85a906965111b1c694b9dab1a566
Danihmn <daniel.bezerra.mult@gmail.com>
2026-01-12 23:01:25 -0300

    Adiciona o Helper que cria a Url dos endpoints

---
+++ RestWithAspNet10/Hypermedia/Utils/UrlHelper.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RestWithAspNet10.Hypermedia.Utils
+{
+    public static class UrlHelper
+    {
+        // Lock object to ensure thread safety
+        private static readonly object _lock = new();
+
+        public static string BuildBaseUrl (this IUrlHelper urlHelper, string routeName, string path)
+        {
+            lock (_lock)
+            {
+                var url = urlHelper.Link(routeName, new { controller = path }) ?? string.Empty;
+                return url.Replace("%2F", "/").TrimEnd('/');
+            }
+        }
+    }
+}
